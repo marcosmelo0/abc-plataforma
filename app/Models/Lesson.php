734f61cab8bcf6be11bf5afb61b56d54c68a15e2
Marcos Melo <marcosmelo012@gmail.com>
2024-09-13 23:03:40 -0300

    Feat: add Api Lesson

---
+++ app/Models/Lesson.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory, UuidTrait;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+    protected $fillable = ['name', 'description', 'videoId'];
+}
