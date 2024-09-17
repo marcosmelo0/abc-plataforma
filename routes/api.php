4a31f203d853b650d4bf230ede5d852e12a2e1b3
Marcos Melo <marcosmelo012@gmail.com>
2024-09-16 21:22:43 -0300

    feat: add replies in supports

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\Api\{
     CourseController,
     LessonController,
     ModuleController,
+    ReplySupportController,
+    SupportController,
 };
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses', [CourseController::class, 'index']);
@@ -16,6 +17,11 @@ Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
 Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
+Route::get('/my-supports', [SupportController::class, 'mySupports']);
+Route::get('/supports', [SupportController::class, 'index']);
+Route::post('/supports', [SupportController::class, 'store']);
+
+Route::post('/replies', [ReplySupportController::class, 'createReply']);
 
 Route::get('/', function() {
     return response()->json([
